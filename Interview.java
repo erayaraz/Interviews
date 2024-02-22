43e672059a85f11260530dffb8c3e14f77119d2e
ErayAraz <erayaraz10@gmail.com>
2024-02-22 16:12:16 +0300

    Interview Solution

---
+++ Interview.java
@@ -0,0 +1,46 @@
+
+public class Interview {
+
+    /**
+     * Coins: 50, 25, 10, 5, 1
+     * Print how many coins value param can have starting from MAX nominal
+     * Example1: 126 - 2x:50 + 1x:25 + 1x:1
+     * Example2: 507 - 10x:50 + 1x:5 + 2x:1
+     * Example3: 999 - 19x:50 + 1x:25 + 2x:10 + 4x:1
+     **/
+    
+    public static void countCoins(int value) {
+        
+        int fiftyCount = getCoinsCounts(50,value);
+
+        int twenyFiveCount = value / 25;
+        value = value - 25 * twenyFiveCount;
+
+        int tenCount = value / 10;
+        value = value - 10 * tenCount;
+
+        int fiveCount = value / 5;
+        value = value - 5 * fiveCount;
+
+        int oneCount = value / 1;
+        value = value - 1 * oneCount;
+
+        System.out.println("50 Count : " + fiftyCount);
+        System.out.println("25 Count : " + twenyFiveCount);
+        System.out.println("10 Count : " + tenCount);
+        System.out.println("5 Count : " + fiveCount);
+        System.out.println("1 Count : " + oneCount);
+
+    }
+
+    private static int getCoinsCounts(final int coinsValue, int value){
+        int count = value / coinsValue;
+        value = value - coinsValue * count;
+        return count;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        countCoins(999);
+    }
+}
